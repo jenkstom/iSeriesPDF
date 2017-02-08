b610e921d6168459bdc5f981f3aac9aed4dbcdf8
Tom White <tom.white@gmail.com>
2017-02-08 15:03:31 -0600

    Add lots of comments and clean out unused code.

--- lpd.cs
+++ lpd.cs
@@ -16,9 +16,9 @@ using System.Threading;
 
 namespace lpd
 {
-    // Thread signal.  
     class lpdaemon
     {
+        // Thread signal.  
         public static ManualResetEvent allDone = new ManualResetEvent(false);
         private static List<StateObject> states = new List<StateObject>();
 
@@ -30,37 +30,70 @@ namespace lpd
         private static Timer timeout;
         private static bool debug = false;
 
+        // Size of receive buffer.  
+        public const int BufferSize = 2048;
+
+        // This state object is the only place we store state
+        // between received packets. It needs to store both
+        // data and flags so that we know what we are doing
+        // when we receive new data.
+        // Effectively we match state to the origin IP/Port.
         public class StateObject
         {
             // Client  socket.  
             public Socket workSocket = null;
-            // Size of receive buffer.  
-            public const int BufferSize = 2048;
             // Receive buffer.  
             public byte[] buffer = new byte[BufferSize];
+
+            // We receive files one packet at a time,
+            // and it may take many packets. So we
+            // store what we have here until it is
+            // complete. 
+
             // Received data info  
+            // Command file tracking
+          
+            // The name of the command file we are tracking
             public String cfName = "";
+            // The command file data
             public StringBuilder cf = new StringBuilder();
+            // This is true while we are receiving a command file
             public bool rcvcf = false;
+
+            // Data file tracking
+            // The name of the data file we are tracking
             public String dfName = "";
+            // The data file
             public StringBuilder df = new StringBuilder();
+            // True when we are receiving a data file
             public bool rcvdf = false;
+
+            // Queue name received from remote. All queues are accepted.
             public String queuename = "";
 
-            //buffer for partial commands
+            // Buffer for partial commands
             public StringBuilder cb = new StringBuilder();
 
+            // Number of bytes we are waiting on for control or data file
             public Int32 bytesremaining = 0;
+
+            // Last time a packet was received for this connection.
+            // Used for timeouts
             public DateTime lastPacket;
+
+            // lpr mode means we are in "subcommand" mode
+            // There is no way to exit subcommand mode in the protocol
             public bool lprmode = false;
         }
 
-
+        //Entry point
         public static void StartLPD()
         {
             log("Starting");
             log("THIS IS A SERVER, PLEASE DO NOT CLOSE THE WINDOW.");
             //**TODO: add smtp user and pass
+
+            //Get config options from .config file
             path = ConfigurationManager.AppSettings["spoolpath"];
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
             smtpserver = ConfigurationManager.AppSettings["smtp"];
@@ -76,6 +109,7 @@ namespace lpd
             // Data buffer for incoming data.  
             byte[] bytes = new Byte[2048];
 
+            // Create timer that calls method every 5 seconds
             timeout = new Timer(timeoutProcessor,null,0,5000);
 
             // Establish the local endpoint for the socket.  
@@ -101,7 +135,6 @@ namespace lpd
                     allDone.Reset();
 
                     // Start an asynchronous socket to listen for connections.  
-                    //log("Waiting for a connection...");
                     listener.BeginAccept(
                         new AsyncCallback(AcceptCallback),
                         listener);
@@ -117,6 +150,7 @@ namespace lpd
             }
         }
 
+        // Close all sockets
         public static void StopLPD()
         {
             log("Shutting down");
@@ -125,19 +159,19 @@ namespace lpd
                 state.workSocket.Shutdown(SocketShutdown.Both);
                 state.workSocket.Close();
             }
-
-            //serverSocket.Close();
-            //todo - kill listener somehow
         }
 
         private static void log(String x)
         {
+            // When this is converted to a Windows service,
+            // this will write to the event log.
             Console.WriteLine($"{DateTime.Now:HH:mm:ss}: {x}");
         }
 
+        // Every five seconds check for and kill all connections
+        // that haven't received a packet for a while.
         private static void timeoutProcessor(object state)
         {
-            //if (debug) log("Timeout check");
             List<StateObject> deleteList = new List<StateObject>();
             foreach (StateObject s in states)
             {
@@ -159,6 +193,7 @@ namespace lpd
                 states.Remove(d);
         }
 
+        // This is called when a client connects to our socket
         public static void AcceptCallback(IAsyncResult ar)
         {
             // Signal the main thread to continue.  
@@ -180,8 +215,13 @@ namespace lpd
                 new AsyncCallback(ReadCallback), state);
         }
 
+        // All incoming packets enter here - except for ACKs
         public static void ReadCallback(IAsyncResult ar)
         {
+            // We are either receiving a file, in which case
+            // bytesremaining will be more than zero, 
+            // or a command. We could receive both in the
+            // same packet, which is why this isn't simple.
             try
             {
                 if (debug) log("Receive packet");
@@ -205,8 +245,12 @@ namespace lpd
                             sb = state.cf;
                         else if (state.rcvdf)
                             sb = state.df;
+
                         if (sb != null)
                         {
+                            // if we have received more data than we are expecting
+                            // only process the data we are expecting and save
+                            // the rest to be processed as a command
                             if (bytesRead >= state.bytesremaining)
                             {
                                 sb.Append(Encoding.ASCII.GetString(
@@ -224,17 +268,13 @@ namespace lpd
                                 if (state.rcvcf)
                                 {
                                     state.rcvcf = false;
-                                    //state.lprmode = false;
                                     if (doConversion(state)) return;
                                 }
                                 if (state.rcvdf)
                                 {
                                     state.rcvdf = false;
-                                    //state.lprmode = false;
                                     if (doConversion(state)) return;
                                 }
-                                //send ack for LPD protocol - not sure if this is necessary
-                                //SendAck(state.workSocket);
                             }
                             else
                             {
@@ -242,33 +282,39 @@ namespace lpd
                                     state.buffer, 0, bytesRead));
                                 state.bytesremaining -= bytesRead;
                             }
-
                         }
-
-                        //command section
-                    } //if bytesremaining>0
+                    } // close of if (bytesremaining>0)
                     else if (bytesRead > 0)
                     {
+                        // Because bytesremaining is not more than zero, 
+                        // this is not file data
+                         
                         //All commands end with linefeed. If there's not 
                         //one, store the current buffer and wait for more data
                         state.cb.Append(Encoding.ASCII.GetString(
                             state.buffer, 0, bytesRead));
+
+                        // If there is a LF we assume it is a command
                         if (state.cb.ToString().IndexOf(Convert.ToChar(10)) >= 0)
                             processCommand(state);
-
                     }
 
-
-                    // Not all data received. Get more.  
+                    // Since our previous packet had >0 bytes, lets wait for more.
                     handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
                     new AsyncCallback(ReadCallback), state);
                 }
-                else //remote closed connection, kill socket
+                else 
                 {
+                    // If we receive a zero byte callback then the 
+                    // remote has closed the connection, kill socket.
                     if (debug) log("Received empty packet, closing connection.");
                     state.workSocket.Shutdown(SocketShutdown.Both);
                     state.workSocket.Close();
                     states.Remove(state);
+                    
+                    // By failing to setup a new BeginReceive / AsyncCallback we will
+                    // never receive another callback for this connection,
+                    // which effectively ends it.
                 }
             }
             catch (Exception ex)
@@ -277,6 +323,8 @@ namespace lpd
             }
         }
 
+        // If we aren't receiving a file and we have received
+        // data, we assume it is a command and arrive here.
         private static void processCommand(StateObject state)
         {
             //Process the command buffer in the state object, remove lines as they are processed
@@ -286,39 +334,43 @@ namespace lpd
             Int32 length;
             String filename;
 
-            //foreach (String cmd in cmdarray)
+            // Commands are terminated with a LF, which is decimal 10 character
             while (cmdbuf.Length > 0 && cmdbuf.IndexOf(Convert.ToChar(10)) >= 0)
             {
+                // Pull out everything up to the first LF
                 cmd = (cmdbuf.Substring(0, cmdbuf.IndexOf(Convert.ToChar(10))));
                 if (debug) log($"Process command: {cmd}");
 
-                //remove the processed command from the command buffer
+                // Remove the processed command from the command buffer
                 state.cb.Remove(0, cmd.Length + 1);
                 cmdbuf = state.cb.ToString();
 
+                // We are either in subcommand (LPR) mode or not
                 if (!state.lprmode)
                 {
-                    //Command mode
+                    // Command mode
+                    // The first byte is the command
+                    // Multibyte to byte encoding is just handled magically
                     switch (Convert.ToByte(cmd[0]))
                     {
-                        case 2: //lpr
+                        case 2: //LPR command
                             state.queuename = cmd.Substring(1).ToUpper();
                             state.lprmode = true;
                             log($"Received LPR Command for queue {state.queuename}");
                             SendAck(state.workSocket);
                             break;
-                        default:
+                        default: //We don't care about other commands
                             Send(state.workSocket, "OK \n");
                             break;
                     }
                 }
                 else
                 {
+                    // We are in subcommand mode
                     length = Convert.ToInt32(cmd.Substring(1, cmd.IndexOf(' ') - 1)) + 1;
                     filename = cmd.Substring(cmd.IndexOf(' ') + 1);
                     switch (Convert.ToByte(cmd[0]))
                     {
-                        //subcommand mode
                         case 2: //control file
                             if (debug) log("Receiving control file");
                             state.cfName = filename;
@@ -377,6 +429,9 @@ namespace lpd
             }
         }
 
+        // This is called after each file is received. But we only take action when
+        // both the command and data files are received completely, and we have 
+        // received a queue name as well.
         private static bool doConversion(StateObject state)
         {
             if (debug) log("Call doConversion()");
@@ -385,9 +440,11 @@ namespace lpd
                 && state.df.Length != 0 && state.queuename != "" && state.cf.Length > 0
                 && state.df.Length > 0)
             {
+                // We are done with the socket, so let's shut it down
                 state.workSocket.Shutdown(SocketShutdown.Both);
                 state.workSocket.Close();
 
+                // The file name is actually a tiny part of this, but we use it all anyway
                 String bfn = Path.GetFileNameWithoutExtension(state.dfName);
                 String username = "";
                 String jobname = "";
@@ -397,6 +454,7 @@ namespace lpd
                 state.cf.Remove(state.cf.Length-1, 1);
                 state.df.Remove(state.df.Length-1, 1);
 
+                // Read the control file into separate lines
                 string[] lines = new StringReader(state.cf.ToString()).ReadToEnd().Split(Convert.ToChar(10));
                 foreach (String l in lines)
                 {
@@ -410,14 +468,15 @@ namespace lpd
                 if (debug) log($"User: {username}");
                 if (debug) log($"job: {jobname}");
 
+                // If we don't have a username we don't know who to email to
+                // We could have a default instead.
                 if (username == "")
                 {
                     log("Can't determine username, aborting.");
                     return true;
                 }
 
-                //log($"Processing received file {bfn}, user {username}, queue {state.queuename}");
-
+                // We finally write the data file to a disk file from memory
                 if (debug) log($"Open streamwriter");
                 StreamWriter datafile = new StreamWriter(Path.Combine(path, state.dfName));
                 datafile.Write(state.df.ToString());
@@ -505,11 +564,16 @@ namespace lpd
                     }
                 }
                 catch { }
+
+                // Free up memory
+                states.Remove(state);
+
                 return true;
             }
             return false;
         }
 
+        // Send an email
         public static void SendMail(String mailTo, String mailFrom, String subject, String body, List<String> attFiles = null)
         {
             //**TODO: Add authentication
@@ -544,7 +608,7 @@ namespace lpd
             }
         }
 
-
+        // Send given data
         private static void Send(Socket handler, String data)
         {
             // Convert the string data to byte data using ASCII encoding.  
@@ -555,17 +619,18 @@ namespace lpd
                 new AsyncCallback(SendCallback), handler);
         }
 
+        // Send an ack
         private static void SendAck(Socket handler)
         {
             try
             {
                 if (debug) log("Sending ack");
                 // Convert the string data to byte data using ASCII encoding.  
-                byte[] ack = new byte[4];
-                ack[0] = ack[2] = ack[3] = 0;
-                ack[1] = 10;
+                byte[] ack = new byte[1];
+                ack[0] = 0;
 
                 // Begin sending the data to the remote device.  
+                // When we receive an ACK the callback executes.
                 handler.BeginSend(ack, 0, 1, SocketFlags.None,
                     new AsyncCallback(SendCallback), handler);
             }
@@ -575,6 +640,8 @@ namespace lpd
             }
         }
 
+        // We have received an ACK for data we send.
+        // We don't actually do anything with it
         private static void SendCallback(IAsyncResult ar)
         {
             try
@@ -586,10 +653,6 @@ namespace lpd
                 // Complete sending the data to the remote device.  
                 int bytesSent = handler.EndSend(ar);
                 if (debug) log($"Remote host acknowledged {bytesSent} bytes.");
-
-                //handler.Shutdown(SocketShutdown.Both);
-                //handler.Close();
-
             }
             catch (Exception e)
             {
